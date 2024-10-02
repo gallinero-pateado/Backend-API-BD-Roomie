9a92449919b6e48d3ffce78192c99e7e8766060f
Aaron Silva <aaronsilva.csl@gmail.com>
2024-10-02 18:57:55 -0300

    Modificacion de modelo y desactivacion de migrar

--- TTSSWW/API/models/usuario.go
+++ TTSSWW/API/models/usuario.go
@@ -1,19 +1,20 @@
 package models
 
+import "time"
+
 type Usuario struct {
-	Id                uint           `gorm:"primaryKey;autoIncrement"`
-	Firebase_usuario  string         `json:"Firebase_usuario"`
-	Roomie            Usuario_Roomie `json:"Usuario_Roomie" gorm:"foreignKey:Id" `
-	Correo            string         `json:"Correo"`
-	Nombres           string         `json:"Nombres"`
-	Apellidos         string         `json:"Apellidos"`
-	Fecha_nacimiento  string         `json:"Fecha_Nacimiento"`
-	Ano_ingreso       string         `json:"Ano_Ingreso"`
-	Id_carrera        uint           `json:"Id_carrera"`
-	Id_estado_usuario bool           `json:"Id_Estado_Usuario"`
-	Foto_perfil       string         `json:"Foto_Perfil"`
-	Fecha_creacion    string         `json:"Fecha_Creacion"`
-	Rol               string         `json:"Rol"`
+	Id                uint      `gorm:"primaryKey;autoIncrement"`
+	Firebase_usuario  string    `gorm:"type:text;uniqueIndex"`
+	Correo            string    `json:"Correo"`
+	Nombres           string    `json:"Nombres"`
+	Apellidos         string    `json:"Apellidos"`
+	Fecha_nacimiento  string    `json:"Fecha_Nacimiento"`
+	Ano_ingreso       string    `json:"Ano_Ingreso"`
+	Id_carrera        uint      `json:"Id_carrera"`
+	Id_estado_usuario bool      `json:"Id_Estado_Usuario"`
+	Foto_perfil       string    `json:"Foto_Perfil"`
+	Fecha_creacion    time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	Rol               string    `json:"Rol"`
 }
 
 func (Usuario) TableName() string {
